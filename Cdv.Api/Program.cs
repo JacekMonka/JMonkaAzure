29a1337018e8030e9d9633c5ae3e7a1de5da8859
Jacek Mońka <jmonka@edu.cdv.pl>
2024-04-18 21:56:41 +0200

    Pierwszy commit: Dodanie plików do repozytorium

---
+++ Cdv.Api/Program.cs
@@ -0,0 +1,39 @@
+using Cdv.Api.Database;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var connectionString = builder.Configuration.GetConnectionString("PeopleDb");
+builder.Services.AddDbContext<PeopleDb>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/people", async (PeopleDb db) =>
+{
+    var people = db.People.ToList();
+
+    return Results.Ok(people);
+});
+
+app.Run();
+
+
