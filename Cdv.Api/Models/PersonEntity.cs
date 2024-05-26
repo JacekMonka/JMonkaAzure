2759ffc89932b0fc1ef2630fd0b6eb7c171e10a6
Jacek <jmonka@edu.cdv.pl>
2024-05-26 14:57:51 +0200

    Usunięcie niepotrzebnych plików

--- Cdv.Api/Models/PersonEntity.cs
+++
@@ -1,9 +0,0 @@
-namespace Cdv.Api.Models;
-
-public class PersonEntity
-{
-    public int PersonId { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string PhoneNumber { get; set; }
-}
